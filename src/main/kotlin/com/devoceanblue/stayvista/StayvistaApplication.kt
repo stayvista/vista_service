f337c53545bc6124e33406b856dfc5c30bc1bbc6
devOceanBlue <87791365+devOceanblue@users.noreply.github.com>
2026-02-07 13:58:48 +0900

    Add project files

---
+++ src/main/kotlin/com/devoceanblue/stayvista/StayvistaApplication.kt
@@ -0,0 +1,11 @@
+package com.devoceanblue.stayvista
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StayvistaApplication
+
+fun main(args: Array<String>) {
+    runApplication<StayvistaApplication>(*args)
+}
---
+++ src/test/kotlin/com/devoceanblue/stayvista/StayvistaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devoceanblue.stayvista
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StayvistaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
